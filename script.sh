0de77912034f75e2528adfb5444f3626af852f0c
Mauricio Valencia <mavalenciaor@unal.edu.co>
2019-05-25 13:37:00 -0500

    Final
    Nota ya generada por __grade__.py

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-﻿result=$(for filename in *.csv
+result=$(for filename in *.csv
 do
  x=0
  while read p
@@ -10,16 +10,23 @@ do
   fi
   var=$(echo "$p" | awk '{gsub(/\t/, ";"); print}' | awk '{gsub(/\t/, ";"); print}' | awk '{sub(/ /, ";"); print}'  | awk '{gsub(/ /, ""); print}' | sed -r '/^\s*$/d' | awk '{split($0,a,";"); print a[2]}')
   varb=$(echo "$p" | awk '{gsub(/\t/, ";"); print}' | awk '{gsub(/\t/, ";"); print}' | awk '{sub(/ /, ";"); print}'  | awk '{gsub(/ /, ""); print}' | sed -r '/^\s*$/d' | awk '{split($0,a,";"); print a[1]}')
-  #echo "$var"
   for element in ${var/,/ }
    do
-   #echo $element
    varc=$(echo "$element" | awk '{split($0,a," "); print a[2]}')
    vard=$(echo "$element" | awk '{split($0,a," "); print a[1]}')
-   #echo $varc
    echo $filename,$x,$varb,$vard
    echo $filename,$x,$varb,$varc
    done
  done < <(grep . "${filename}")
 done)
-echo $result | awk '{gsub(/ /, ","); print}' | awk '/\:/{print}'
+resultb=$(echo "$result" | sed 's/\r//g' | awk '{gsub(/ /, ","); print}' | awk '/\:/{print}')
+
+for line in ${resultb}
+do
+IFS=',' read -a myarray <<< "$line"
+#echo "${#myarray[@]}"
+for((c=3; c<"${#myarray[@]}"; c++))
+do
+    echo "${myarray[0]}","${myarray[1]}","${myarray[2]}","${myarray[$c]}" 
+done
+done
